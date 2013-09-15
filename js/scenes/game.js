8713079cc8bd5f1f6742b3443a08d4ddd4b3ae10
billnreed <billnreed@gmail.com>
2013-09-15 14:18:57 -0700

    redo file formatting

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -1,36 +1,40 @@
-define(["crafty", 
-        "spawners/suicide-enemy-spawner",
-        "components/destroy-offstage",
-        "components/move-straight",
-        "components/player", 
-        "components/bullet", 
-        "components/suicide-enemy", 
-        "components/takes-damage",
-        "components/gives-damage"
-        ], function(Crafty, SuicideEnemyFactory) {
+define(
+        [
+            "crafty",
+            "spawners/suicide-enemy-spawner",
+            "components/destroy-offstage",
+            "components/move-straight",
+            "components/player",
+            "components/bullet",
+            "components/suicide-enemy",
+            "components/takes-damage",
+            "components/gives-damage"
+        ],
+        function(Crafty, SuicideEnemyFactory) {
 
-    var spawner = null;
-    var player = null;
-    Crafty.scene("Game", function() {
-        Crafty.background('rgb(0,100,200)');
+            var spawner = null;
+            var player = null;
+            Crafty.scene("Game", function() {
+                Crafty.background('rgb(0,100,200)');
 
-        player = Crafty.e("Player");
-        Crafty.addEvent(player, Crafty.stage.elem, "mouseup", player.fire);
+                player = Crafty.e("Player");
+                Crafty.addEvent(player, Crafty.stage.elem, "mouseup", player.fire);
 
-        spawner = Crafty.e("SuicideEnemySpawner")
-                            .suicideEnemySpawner(player, {
-                                minInterval: 500,
-                                maxInterval: 1250,
-                                minHealth: 5,
-                                maxHealth: 10,
-                                minSpeed: 3,
-                                maxSpeed: 4,
-                                minDamage: 5,
-                                maxDamage: 5
-                            });
-        spawner.start();
-    }, function() {
-        spawner.stop();
-        Crafty.removeEvent(player, Crafty.stage.elem, "mouseup", player.fire);
-    });
-});
+                spawner = Crafty.e("SuicideEnemySpawner")
+                        .suicideEnemySpawner(player, {
+                    minInterval: 500,
+                    maxInterval: 1250,
+                    minHealth: 5,
+                    maxHealth: 10,
+                    minSpeed: 3,
+                    maxSpeed: 4,
+                    minDamage: 5,
+                    maxDamage: 5
+                });
+                spawner.start();
+            }, function() {
+                spawner.stop();
+                Crafty.removeEvent(player, Crafty.stage.elem, "mouseup", player.fire);
+            });
+        }
+);
