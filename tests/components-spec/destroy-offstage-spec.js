1780a0f39a2693ac68d3854d071f591dab2afdd5
billnreed <billnreed@gmail.com>
2013-09-15 17:37:03 -0700

    fixed a unit test

--- tests/components-spec/destroy-offstage-spec.js
+++ tests/components-spec/destroy-offstage-spec.js
@@ -8,6 +8,7 @@ define(["crafty", "components/destroy-offstage"], function(Crafty) {
         it("is not destroyed while onstage", function() {
             var destroyOffstage = Crafty.e("DestroyOffstage")
                                         .attr({x: 100, y: 100, w: 100, h: 100});                    
+            destroyOffstage._destroyOffstage();
             destroyOffstage.shift(10, 10);
             destroyOffstage._destroyOffstage();
 
@@ -16,16 +17,18 @@ define(["crafty", "components/destroy-offstage"], function(Crafty) {
 
         it("is not destroyed when starting offstage", function() {
             var destroyOffstage = Crafty.e("DestroyOffstage")
-                                        .attr({x: -110, y: -110, w: 100, h: 100});           
+                                        .attr({x: -110, y: -110, w: 100, h: 100});
+            destroyOffstage._destroyOffstage();
             destroyOffstage.shift(200, 200);
             destroyOffstage._destroyOffstage();
 
             expect(Crafty("DestroyOffstage").length).toBe(1);
         });
 
-        xit("is destroyed when moved offstage", function() {
+        it("is destroyed when moved offstage", function() {
             var destroyOffstage = Crafty.e("DestroyOffstage")
                                         .attr({x: 100, y: 100, w: 100, h: 100});
+            destroyOffstage._destroyOffstage();
             destroyOffstage.shift(1000, 1000);
             destroyOffstage._destroyOffstage();
 
